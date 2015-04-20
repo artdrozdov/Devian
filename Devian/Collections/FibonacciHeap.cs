90d388de18d0d4629d2750a15d74ebc0447a92da
artdrozdov <artem.drozdoff@gmail.com>
2015-04-20 20:33:32 +0300

    Linked List

---
+++ Devian/Collections/FibonacciHeap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devian.Collections
+{
+    class FibonacciHeap<T> : ICollection<T> where T : IComparable<T> 
+    {
+
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        bool ICollection<T>.Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        int ICollection<T>.Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool ICollection<T>.IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        bool ICollection<T>.Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevianTests/LinkedListTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Devian.Collections;
+using System.Linq;
+
+namespace DevianTests
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void LinkedListAdd()
+        {
+            var lst = new LinkedList<int>();
+            lst.Add(1);
+            lst.Add(1);
+            lst.Add(1);
+            foreach (var i in lst)
+            {
+                Assert.AreEqual(1, i);
+            }
+            Assert.AreEqual(3, lst.Count);
+        }
+
+        [TestMethod]
+        public void LinkedListContains()
+        {
+            var lst = new LinkedList<int>();
+            lst.Add(2);
+            Assert.AreEqual(true, lst.Contains(2));
+        }
+        
+        [TestMethod]
+        public void LinkedListRemove()
+        {
+            var lst = new LinkedList<int>();
+            lst.Add(2);
+            lst.Remove(2);
+            Assert.AreEqual(0, lst.Count);
+        }
+
+        [TestMethod]
+        public void LinkedListLinq()
+        {
+            var lst = new LinkedList<int>();
+            lst.Add(1);
+            lst.Add(2);
+            Assert.AreEqual(true, lst.Any(x => x == 2));
+        }
+    }
+}
