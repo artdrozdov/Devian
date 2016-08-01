12a794a8c41eee8bd3bb8c07daf2d36cd4d67483
Artem Drozdov <ardr@Betsson.local>
2016-08-01 11:04:20 +0300

    extensions

---
+++ Devian/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Devian
+{
+    static class Extensions
+    {
+        public static string ToMd5(this string self)
+        {
+            using (var algorithm = MD5.Create())
+            {
+                var byteArray = algorithm.ComputeHash(Encoding.UTF8.GetBytes(self));
+                var resultBuilder = new StringBuilder();
+                foreach (byte bin in byteArray)
+                {
+                    resultBuilder.Append(bin.ToString("x2"));
+                }
+                return resultBuilder.ToString();
+            }
+        }
+    }
+}
